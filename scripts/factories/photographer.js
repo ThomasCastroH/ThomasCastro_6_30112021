4e27d5cfd47b4675f63c49c955b63491c02f68f6
Thomas Castro <contact.thomas.Castro@gmail.com>
2021-12-09 18:01:12 +0100

    Jquery + fetch les infos des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,25 +5,33 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        article.setAttribute("id", id);
+        article.setAttribute("tabindex", "0");
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "picture of the photographer");
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
         const span = document.createElement( 'span' );
-        const h3 = document.createElement( 'h3' );
-        h3.textContent = city +', ' + country;
+
+        const location = document.createElement( 'h3' );
+        location.textContent = city +', ' + country;
+
         const paragraphe = document.createElement( 'p' );
         paragraphe.textContent = tagline;
+
         const rate = document.createElement( 'p' );
         rate.textContent = price + "€/jour";
+
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(span);
-        span.appendChild(h3);
+        span.appendChild(location);
         span.appendChild(paragraphe);
         span.appendChild(rate);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, city, country, tagline, price, getUserCardDOM }
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,16 +1,86 @@
-// DOM Elements
-const modalBtn = document.querySelectorAll(".contact_button");
-const modal = document.getElementById("contact_modal");
+// Regex
+const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const nameRegex = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+
+// Inputs
+const inputIn = document.querySelectorAll('.text-control');
 
 // open modal
 function displayModal() {
+    const modal = document.getElementById("contact_modal");
 	modal.style.display = "flex";
 }
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", displayModal));
 
+// close modal
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
 
+// Eventslisteners for all inputs
+inputIn[0].addEventListener("change", isFirstnameValid);
+inputIn[1].addEventListener("change", isLastnameValid);
+inputIn[2].addEventListener("change", isEmailValid);
+inputIn[3].addEventListener("change", isMessageValid);
+
+// Functions
+// check if firstname is valid
+
+function isFirstnameValid() {
+    if (nameRegex.test(inputIn[0].value)) {
+        inputIn[0].parentElement.setAttribute("data-error-visible", "false");
+        return true;
+    } else {
+        inputIn[0].parentElement.setAttribute("data-error-visible", "true");
+        return false;
+    }
+}
+  
+// check if lastname is valid
+function isLastnameValid() {
+    if (nameRegex.test(inputIn[1].value)) {
+        inputIn[1].parentElement.setAttribute("data-error-visible", "false");
+        return true;
+    } else {
+        inputIn[1].parentElement.setAttribute("data-error-visible", "true");
+        return false;
+    }
+}
+  
+// check if email adress is valid
+function isEmailValid() {
+    if (emailRegex.test(inputIn[2].value)) {
+        inputIn[2].parentElement.setAttribute("data-error-visible", "false");
+        return true;
+    } else {
+        inputIn[2].parentElement.setAttribute("data-error-visible", "true");
+        return false;
+    }
+}
+
+// check if message is valid
+function isMessageValid() {
+if (inputIn[3].value !== "") {
+    inputIn[3].parentElement.setAttribute("data-error-visible", "false");
+    return true;
+    } else {
+    inputIn[3].parentElement.setAttribute("data-error-visible", "true");
+    return false;
+    }
+}
+
+// check if all the form is valid
+function isFormValid(e) {
+    e.preventDefault();
+    if (isFirstnameValid() && isLastnameValid() && isEmailValid() && isMessageValid()) {
+      form.submit();
+    } else {
+      isFirstnameValid();
+      isLastnameValid();
+      isEmailValid();
+      isMessageValid();
+    }
+}
+
+// validation event
+form.addEventListener("submit", isFormValid);
