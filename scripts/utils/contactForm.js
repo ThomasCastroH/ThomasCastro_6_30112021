d291acc388172d98c6554291a541fb913830967c
Thomas Castro <contact.thomas.Castro@gmail.com>
2021-12-09 16:07:58 +0100

    mise en palce de la modal de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,16 @@
+// DOM Elements
+const modalBtn = document.querySelectorAll(".contact_button");
+const modal = document.getElementById("contact_modal");
+
+// open modal
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+	modal.style.display = "flex";
 }
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", displayModal));
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
